b20cab582e890590e11bf097cc335e570e599896
wikiselev <vk6@sanger.ac.uk>
2019-01-04 14:51:47 +0000

    update port number

--- treg-tomas/ss2_human/docker.R
+++ treg-tomas/ss2_human/docker.R
@@ -2,7 +2,7 @@ timestamp()
 message("* Started")
 
 out <- source("app.R")
-shiny::runApp(out$value, port = 8001, host = "0.0.0.0")
+shiny::runApp(out$value, port = 1234, host = "0.0.0.0")
 
 message("* Completed")
 timestamp()
