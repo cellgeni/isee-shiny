10a287785eb9b5151b5cfb7320a7c762eac03b36
Anton Khodak <anton.khodak@ukr.net>
2019-01-22 09:28:36 +0000

    Fix download script

--- k8s/get-data.sh
+++ k8s/get-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-FILE=lkjlkj     
-if [ -f $FILE ]; then
-   ./gdown.pl https://drive.google.com/file/d/1JCdcoGZH2J0TGvcs0pqbRb7s05Aucevj/view?usp=sharing /data/human/sce.rds
-   ./gdown.pl https://drive.google.com/file/d/1mFakW6KKunx7ogZvzaETbC1ayyI7vIK8/view?usp=sharing /data/human/colourmap.rds 
-fi
+# FILE=lkjlkj     
+# if [ -f $FILE ]; then
+   ./gdown.pl https://drive.google.com/file/d/1JCdcoGZH2J0TGvcs0pqbRb7s05Aucevj/view?usp=sharing /data/treg-tomas/human/sce.rds
+   ./gdown.pl https://drive.google.com/file/d/1mFakW6KKunx7ogZvzaETbC1ayyI7vIK8/view?usp=sharing /data/treg-tomas/human/colourmap.rds 
+# fi
 
